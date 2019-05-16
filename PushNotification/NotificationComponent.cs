11c790121b6bd262d619f6ec5f5b4332c0b5ad87
Salahuddin Ahmed <sumu2007@gmail.com>
2019-05-16 14:06:10 +0600

    Removing bin and obj folder along with suo file; also, removing unnecessary namespaces

--- PushNotification/NotificationComponent.cs
+++ PushNotification/NotificationComponent.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Data.SqlClient;
 using Microsoft.AspNet.SignalR;
 using PushNotification.Models;
